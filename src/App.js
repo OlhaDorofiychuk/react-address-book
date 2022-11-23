270f95d644182b8269925f9fdd24fd980063b1c6
OlhaDorofiychuk <olha.d@yahoo.com>
2022-11-23 00:24:56 +0000

    ring loader is added to main page for now

--- src/App.js
+++ src/App.js
@@ -7,83 +7,86 @@ import ContactEdit from "./components/ContactEdit";
 import "./styles/styles.css";
 import Meeting from "./components/Meeting";
 import MeetingsList from "./components/MeetingsList";
+import { Rings } from "react-loader-spinner";
 
 export default function App({ meetingsList, setMeetingsList }) {
   const [contacts, setContacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   //Request to get all contacts from the database
   useEffect(() => {
+    setIsLoading(true);
     fetch("http://localhost:4000/contacts")
       .then((res) => res.json())
-      .then((contactData) => setContacts(contactData));
+      .then((contactData) => {
+        setTimeout(() => {
+          setContacts(contactData);
+          setIsLoading(false);
+        }, 2000);
+      });
   }, []);
 
   //TODO: Load all contacts on useEffect when component first renders
   //contacts/?type"
-  let workContacts = [];
-  let personalContacts = [];
-  console.log("contacts before 23", contacts);
-  contacts.map((contact) => {
-    if (contact.type === "work") {
-      workContacts.push(contact);
-    }
-    if (contact.type === "personal") {
-      personalContacts.push(contact);
-    }
-  });
-  console.log("all contacts", contacts);
-  console.log("work", workContacts);
-  console.log("personal", personalContacts);
+
   return (
     <>
-      <nav>
-        <h2>Menu</h2>
-        <ul>
-          {/* TODO: Make these links */}
-          <li>
-            <Link to="/">Contacts List</Link>
-          </li>
-          <li>
-            <Link to="/contacts/add">Add New Contact</Link>
-          </li>
-          <li>
-            <Link to="/meetings">Meetings</Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <Routes>
-          <Route path="/" element={<ContactsList contacts={contacts} />} />
-          <Route path="/meetings" element={<MeetingsList />} />
-          <Route
-            path="/contacts/:id/meetings"
-            element={
-              <Meeting
-                meetingsList={meetingsList}
-                setMeetingsList={setMeetingsList}
+      {isLoading ? (
+        <div className="container">
+          <Rings color="#00BFFF" height={300} width={300} />
+        </div>
+      ) : (
+        <>
+          <nav>
+            <h2>Menu</h2>
+            <ul>
+              {/* TODO: Make these links */}
+              <li>
+                <Link to="/">Contacts List</Link>
+              </li>
+              <li>
+                <Link to="/contacts/add">Add New Contact</Link>
+              </li>
+              <li>
+                <Link to="/meetings">Meetings</Link>
+              </li>
+            </ul>
+          </nav>
+          <main>
+            <Routes>
+              <Route path="/" element={<ContactsList contacts={contacts} />} />
+              <Route path="/meetings" element={<MeetingsList />} />
+              <Route
+                path="/contacts/:id/meetings"
+                element={
+                  <Meeting
+                    meetingsList={meetingsList}
+                    setMeetingsList={setMeetingsList}
+                  />
+                }
               />
-            }
-          />
-          <Route
-            path="/contacts/add"
-            element={
-              <ContactsAdd contacts={contacts} setContacts={setContacts} />
-            }
-          />
-          <Route
-            path="/contacts/:id"
-            element={
-              <ContactsView contacts={contacts} setContacts={setContacts} />
-            }
-          />
-          <Route
-            path="/contacts/:id/edit"
-            element={
-              <ContactEdit contacts={contacts} setContacts={setContacts} />
-            }
-          />
-        </Routes>
-      </main>
+              <Route
+                path="/contacts/add"
+                element={
+                  <ContactsAdd contacts={contacts} setContacts={setContacts} />
+                }
+              />
+              <Route
+                path="/contacts/:id"
+                element={
+                  <ContactsView contacts={contacts} setContacts={setContacts} />
+                }
+              />
+              <Route
+                path="/contacts/:id/edit"
+                element={
+                  <ContactEdit contacts={contacts} setContacts={setContacts} />
+                }
+              />
+            </Routes>
+          </main>
+        </>
+      )}
     </>
   );
 }
